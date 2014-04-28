875af36692fc49181dda06250504ee7f39062ccf
Jerry Walsh <jerry.walsh@gmail.com>
2014-04-28 22:13:48 +1200

    cleaner way of doing it

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -1,15 +1,21 @@
-Spree::BaseController.class_eval do
+module CMSControllerHelper
+  def self.included(base)
+    base.class_eval do
+      before_filter :get_pages
+      helper_method :current_page
+    end
+    
+    def current_page
+      @page ||= Spree::Page.find_by_path(request.fullpath.to_s.gsub(/\?.*$/, ""))
+    end
+    
+    def get_pages
+      return if request.path =~ /^\/+admin/
+      @pages ||= Spree::Page.visible.order(:position).all
+    end
 
-  before_filter :get_pages
-  helper_method :current_page
-  
-  def current_page
-    @page ||= Spree::Page.find_by_path(request.fullpath.to_s.gsub(/\?.*$/, ""))
   end
-  
-  def get_pages
-    return if request.path =~ /^\/+admin/
-    @pages ||= Spree::Page.visible.order(:position).all
-  end
-
 end
+
+Spree::BaseController.send :include, CMSControllerHelper
+Devise::SessionsController.send :include, CMSControllerHelper
